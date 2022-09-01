da56ce8b5dbb1398e49db02defe9abdee9212983
livingdead311 <109907237+livingdead311@users.noreply.github.com>
2022-09-01 21:07:36 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "5696987909:AAEfU1jA4yHrOA2RjmrBU-pqrQIdl9Afa2s"
+
+keys = {
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'юань': 'CNY',
+    'йена': 'JPY',
+    'тенге': 'KZT',
+    'золото': 'GLD'
+}
